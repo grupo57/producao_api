b3349cd3dbeef83f9e2c492a3bef54c07decda44
Fábio Peixoto <fabio.silva@empresta.com.br>
2024-12-01 18:27:25 -0300

    fix: Ajustando o nome do arquivo

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = file("userdata.tpl")
+  user_data = file("userdata.sh")
 }
 
 resource "aws_security_group" "web-sg" {
