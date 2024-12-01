bbab073a1c8d5231baa8a8170d0f2848fd6ea580
Fábio Peixoto <fabio.silva@empresta.com.br>
2024-12-01 18:24:50 -0300

    feat: Adicionando o deploy da aplicação

--- main.tf
+++ main.tf
@@ -51,14 +51,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "<style> body {background-color: black;}</style><img style="text-align:center" src="https://postech.fiap.com.br/gifs/loader.gif"><img src="https://postech.fiap.com.br/imgs/fiap-plus-alura/fiap_alura.png">" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+  user_data = file("userdata.tpl")
 }
 
 resource "aws_security_group" "web-sg" {
